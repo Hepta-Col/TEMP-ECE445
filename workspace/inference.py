016914403fcdf3affeb68c4af01cbb19f496fa49
magicbook <zhenting.19@intl.zju.edu.cn>
2023-04-24 21:22:29 +0800

    system on magicbook

--- workspace/inference.py
+++ workspace/inference.py
@@ -3,10 +3,16 @@ import torch
 from common.args import get_args
 from utils.System import System
 
+import warnings
+warnings.filterwarnings('ignore')
+
 
 def main():
     args = get_args()
     system = System(args)
+    
+    data = torch.ones((24, 5))
+    a, b = system.predict_single_step(data)
     
 
 if __name__ == '__main__':
