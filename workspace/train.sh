17a93f78c56ab710b33ff2763ee220d53d88f3e7
magicbook <zhenting.19@intl.zju.edu.cn>
2023-05-10 14:04:45 +0800

    new

--- workspace/train.sh
+++ workspace/train.sh
@@ -1,3 +1,3 @@
-CUDA_VISIBLE_DEVICES=0 python train.py --train_classifier --debug
+CUDA_VISIBLE_DEVICES=0 python train.py --train_classifier --save_models
 # CUDA_VISIBLE_DEVICES=0 python train.py --granularity hour --train_forecaster --save_models 
 # CUDA_VISIBLE_DEVICES=0 python train.py --granularity day --train_forecaster --save_models 
