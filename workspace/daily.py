a99b7296f2885ad2b5f658990afb3c6901700bbc
magicbook <zhenting.19@intl.zju.edu.cn>
2023-05-15 18:33:03 +0800

    new

--- workspace/daily.py
+++ workspace/daily.py
@@ -10,21 +10,25 @@ from utils.System import System
 
 
 def main():
+    print("==> Creating forecasting system...")
     system = System(args)
-    test_data = pd.read_csv(args.test_path)
-    assert test_data.shape == (48, 5)
-    test_data = torch.tensor(test_data.values)
+    
+    test_hour_data = pd.read_csv("")
+    test_day_data = pd.read_csv("")
+    assert test_hour_data.shape == (48, 5) and test_day_data.shape == (48, 5)
+    test_hour_data = torch.tensor(test_hour_data.values)
+    test_day_data = torch.tensor(test_day_data.values)
+    
     month_col = torch.tensor([float(datetime.now().month) for _ in range(48)]).unsqueeze(1)
-    model_input = torch.cat((test_data, month_col), dim=1)
+    model_input = torch.cat((test_day_data, month_col), dim=1)
     assert model_input.shape == (48, 6)
     predictions = system.predict_multi_step(model_input, args.prediction_length)
     
     for p in predictions:
         print(p)
-    
 
 
 if __name__ == '__main__':
     main()
     print("test.py: DONE!")
-    open(os.path.join(args.out_root, "test-DONE"), "w").close()
+    open(os.path.join(args.out_root, "run-DONE"), "w").close()
